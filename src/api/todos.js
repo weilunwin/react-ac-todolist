12f6c4fc09b1748a4163b15840594efb31672017
weilunwin <weilunwin@gmail.com>
2023-11-18 16:05:57 +0800

    no message

--- src/api/todos.js
+++ src/api/todos.js
@@ -1,11 +1,27 @@
 import axios from 'axios';
-const baseUrl = 'http://localhost:3001';
+const baseUrl = 'https://todo-list.alphacamp.io/api';
+const axiosInstance = axios.create({
+  baseURL: baseUrl,
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('authToken');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    console.error(error);
+  },
+);
 
 //取得資料
 export const getTodos = async () => {
   try {
-    const res = await axios.get(`${baseUrl}/todos`);
-    return res.data;
+    const res = await axiosInstance.get(`${baseUrl}/todos`);
+    return res.data.data;
   } catch (error) {
     console.error('[Get Todo failed]: ', error);
   }
@@ -14,7 +30,7 @@ export const getTodos = async () => {
 export const createTodo = async (payload) => {
   const { title, isDone } = payload;
   try {
-    const res = await axios.post(`${baseUrl}/todos`, { title, isDone });
+    const res = await axiosInstance.post(`${baseUrl}/todos`, { title, isDone });
     return res.data;
   } catch (error) {
     console.error('[Create Todo failed]: ', error);
@@ -25,7 +41,10 @@ export const patchTodo = async (payload) => {
   const { id, title, isDone } = payload;
 
   try {
-    const res = await axios.patch(`${baseUrl}/todos/${id}`, { title, isDone });
+    const res = await axiosInstance.patch(`${baseUrl}/todos/${id}`, {
+      title,
+      isDone,
+    });
     return res.data;
   } catch (error) {
     console.error('[Patch Todo failed]: ', error);
@@ -34,7 +53,7 @@ export const patchTodo = async (payload) => {
 //刪除資料
 export const deleteTodo = async (id) => {
   try {
-    const res = await axios.delete(`${baseUrl}/todos/${id}`);
+    const res = await axiosInstance.delete(`${baseUrl}/todos/${id}`);
     return res.data;
   } catch (error) {
     console.error('[Delete Todo failed]: ', error);
